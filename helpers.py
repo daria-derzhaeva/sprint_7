8d2d98a2c683ccd53bee0b2d36d2e29aa1b93fc5
Daria <derjaeva160@gmail.com>
2024-12-20 22:12:58 +0500

    далены все файлы, кроме README.md

--- helpers.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-def generate_random_login(length=8):
-    letters = string.ascii_lowercase
-    login = ''.join(random.choice(letters) for i in range(length))
-    return login
